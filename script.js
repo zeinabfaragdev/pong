1c58376fd587bd31f02d909e6095b585e474cf61
Zeinab Farag <zeinabfarag454@gmail.com>
2021-12-23 02:46:50 -0500

    adding game logic

--- script.js
+++ script.js
@@ -85,9 +85,6 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Remove this
-createCanvas();
-
 // Reset Ball to Center
 function ballReset() {
   ballX = width / 2;
@@ -200,6 +197,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -214,8 +212,8 @@ function startGame() {
   ballReset();
   createCanvas();
   animate();
+
   canvas.addEventListener("mousemove", (e) => {
-    console.log(e.clientX);
     playerMoved = true;
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
@@ -231,4 +229,4 @@ function startGame() {
 }
 
 // On Load
-// startGame();
+startGame();
